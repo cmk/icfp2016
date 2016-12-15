cd799cb11be4324ecaf73242df3be137458e22ef
chris <chris@datascience.com>
2016-12-14 16:47:00 -0800

    test & clean up.

--- parser.hs
+++ parser.hs
@@ -3,6 +3,12 @@ import Data.Ratio
 import Text.Regex (splitRegex, mkRegex)
 import Text.Read
 
+type Point = (Rational, Rational)
+type LineSeg = (Point, Point)
+type Polygon = [Point]
+type Silhouette = [Polygon]
+type Skeleton = [LineSeg]
+type Problem = (Silhouette, Skeleton)
 type Parse a = State [String] a
 
 tokenize :: String -> [String]
@@ -24,6 +30,43 @@ runParseRational (x:xs) =
       (n:d) = split x
       split = splitRegex (mkRegex "/")
   in (r,xs)
-  
+
+parsePoint :: Parse Point
+parsePoint = fmap (\[x,y] -> (x,y)) (repeatP 2 parseRational)
+
+parseLineSeg :: Parse LineSeg
+parseLineSeg = fmap (\[x,y] -> (x,y)) (repeatP 2 parsePoint)
+
+parsePolygon :: Parse Polygon
+parsePolygon = parseNum >>= (\n -> repeatP n parsePoint)
+
+parseSilhouette :: Parse Silhouette
+parseSilhouette = parseNum >>= (\n -> repeatP n parsePolygon)
+
+parseSkeleton :: Parse Skeleton
+parseSkeleton = parseNum >>= (\n -> repeatP n parseLineSeg)
+
+parseProblem :: Parse Problem
+parseProblem = do
+  sil <- parseSilhouette
+  skel <- parseSkeleton
+  return (sil,skel)
+
+--From the original problem spec here: http://icfpc2016.blogspot.jp/
+exampleProb =
+  "1 \
+\4 \
+\0,0 \
+\1,0 \
+\1/2,1/2 \
+\0,1/2 \
+\5 \
+\0,0 1,0 \
+\1,0 1/2,1/2 \
+\1/2,1/2 0,1/2 \
+\0,1/2 0,0 \
+\0,0 1/2,1/2"
+
+
 main :: IO ()
-main = print $ show $ runState (repeatP 4 parseNum) (tokenize "0,0 1,0")
+main = print $ show $ fst $ runState parseProblem (tokenize exampleProb)
